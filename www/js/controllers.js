b395b575d6eb2c7e926608b650e1539000dc8ee1
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-22 17:03:21 -0300

    resize done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,48 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $cordovaImagePicker, $cordovaFileTransfer) {
+  $scope.getPicture = function() {
+    var options = {
+      maximumImagesCount: 1
+    };
+
+    //http://www.raymondcamden.com/2015/03/12/selecting-multiple-images-in-a-phonegapcordova-app/
+    $cordovaImagePicker.getPictures(options).then(function(results) {
+      $scope.originalImageUri = results[0];
+
+      /*$cordovaFileTransfer.download(results[0], cordova.file.dataDirectory + 'image.jpg', {}, true).then (
+        function(fileEntry) {
+          $scope.originalImageUri = fileEntry.nativeURL;
+          console.log('Original image URI: ' + $scope.originalImageUri);
+        }, function (error) {
+          console.log(error);
+        }
+      );*/
+    }, function(error) {
+      console.log('Error trying to load the image - ' + JSON.stringify(error));    // In case of error
+    });
+  };
+
+  $scope.resizePicture = function() {
+    var resizedImageUri = $scope.originalImageUri.replace(/(\.[\w\d_-]+)$/i, '_resized$1');
+
+    var options = {
+      sourceUri: $scope.originalImageUri,
+      destinationUri: resizedImageUri,
+      newWidth: 100,
+      newHeight: 400,
+      keepScale: true
+    };
+
+    ImageProcessing.resize(function(success) {
+      $scope.resizedImageUri = resizedImageUri;
+
+      console.log('Image resized!! - URI: ' + resizedImageUri);
+    }, function(error) {
+      console.log('Error trying to resize the image - ' + JSON.stringify(error));    // In case of error
+    }, options)
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
